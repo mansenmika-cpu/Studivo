a3636dd90fd48ed21b8e9fab5907c4c565e21d41
Thisara Adhikarinayake <mansenmika@gmail.com>
2026-01-21 15:38:38 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-start = st.Page("start.py", title="start")
-home = st.Page("home.py", title="Home")
-modules = st.Page("modules.py", title="Manage Modules")
-chatbot = st.Page("chatbot.py", title="Chat Bot")
-tests = st.Page("tests.py", title="Tests")
+start = st.Page("start.py", title="Start", icon=":material/play_circle:")
+home = st.Page("home.py", title="Home", icon=":material/home:")
+modules = st.Page("modules.py", title="Manage Modules", icon=":material/library_books:")
+chatbot = st.Page("chatbot.py", title="Chat Bot", icon=":material/smart_toy:")
+tests = st.Page("tests.py", title="Tests", icon=":material/quiz:")
 
 pg = st.navigation([home, modules, chatbot, tests])
 
